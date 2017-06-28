30e26dd94d284df6c0e32d65b6284679cc02154b
Unknown <teamsloslo@gmail.com>
2017-06-28 13:39:35 +0200

    Added structure for all exercises

--- instructor/nbgrader_config.py
+++ instructor/nbgrader_config.py
@@ -3,8 +3,12 @@ from nbgrader.utils import get_username
 c = get_config()
 
 c.NbGrader.course_id = "KJM1101"
-c.NbGrader.db_assignments = [dict(name="lab1")]
+c.NbGrader.db_assignments = [dict(name="lab1"), 
+							 dict(name="lab2"),
+							 dict(name="lab3"),
+							 dict(name="lab4"),
+							 dict(name="lab5"),]
 c.NbGrader.db_students = [dict(id=get_username())]
 
-c.ClearSolutions.code_stub = {"python": "# SKRIV DITT SVAR HER\nraise NotImplementedError"}
+c.ClearSolutions.code_stub = {"python": "# SKRIV DITT SVAR HER"}
 
